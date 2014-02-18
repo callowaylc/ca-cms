757b51ba8a5a59b18ba9970eb885909258a6fb8b
Ubuntu <ubuntu@ip-10-73-188-247.ec2.internal>
2014-02-18 01:25:06 +0000

    added term resource to api

---
+++ wp-api/term/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * "Controller" for term resource
+ *
+ * @package WordPress
+ * @subpackage Administration
+ */
+
+const TAXONOMY = 'category';
+
+// iterate through tags and build listing of images
+$data = [ ];
+
+foreach($_REQUEST['terms'] as $parent) { 
+	$parent   = get_term_by('name', $parent, TAXONOMY);
+  $children = get_term_children($parent->term_id, TAXONOMY);
+	
+  foreach($children as $id) {
+    $data[] = (array)get_term_by('id', $id, TAXONOMY);
+  }
+}
+
+// finally encode and "return" response
+echo json_encode(array_values($data));
