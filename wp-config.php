5fe8f7bc03fbfbfad4743ef0f728286eceb791b3
christian calloway <ccalloway@cancapital.com>
2014-11-30 01:00:47 -0500

    changed abs path env to app cms

--- wp-config.php
+++ wp-config.php
@@ -6,13 +6,13 @@ require_once './bootstrap.php';
 /* Default value for some constants if they have not yet been set
    by the host-specific config files */
 if (!defined('ABSPATH'))
-    define('ABSPATH', '/var/www/cms');
+    define('ABSPATH', '/app/cms');
 if (!defined('WP_CORE_UPDATE'))
     define('WP_CORE_UPDATE', false);
 if (!defined('WP_ALLOW_MULTISITE'))
     define('WP_ALLOW_MULTISITE', false);
 if (!defined('WP_CONTENT_DIR') && !defined('DONT_SET_WP_CONTENT_DIR'))
-    define('WP_CONTENT_DIR', '/var/www/cms/wp-content');
+    define('WP_CONTENT_DIR', ABSPATH . '/wp-content');
 
 define('FS_METHOD', 'direct');
 /* Default value for the table_prefix variable so that it doesn't need to
